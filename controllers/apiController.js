6a69f4bd27a219a12b1e03468bb06fb359a1fe82
Molly Gibbs <mollygibbs@Molly-Gibbss-MacBook-Pro-2.local>
2022-05-24 16:09:35 -0300

    api

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,16 +1,92 @@
 const { Article, User, Comment } = require("../models");
 const { Op } = require("sequelize");
 
+//ARTICLES
 //Show all articles
 async function indexArticles(req, res) {
   const articles = await Article.findAll({
-    include: User,
     raw: true,
     order: [["createdAt", "DESC"]],
   });
   res.json(articles);
 }
 
+//Filter by Author(User)
+async function FindArticleByUserId(req, res) {
+  const articles = await Article.findAll({
+    where: { userId: req.params.id },
+    raw: true,
+    order: [["createdAt", "DESC"]],
+  });
+  res.json(articles);
+}
+
+//Filter by title
+async function findArticleByTitle(req, res) {
+  const articles = await Article.findAll({
+    where: {
+      title: {
+        [Op.substring]: [req.params.title],
+      },
+    },
+    raw: true,
+    order: [["createdAt", "DESC"]],
+  });
+  res.json(articles);
+}
+
+//Create new article (with title and content)
+async function createArticle(req, res) {
+  if (!req.body.title || !req.body.content) {
+    res.send("You need to provide a title and some content.");
+  }
+
+  const article = {
+    title: req.body.title,
+    content: req.body.content,
+  };
+
+  const newArticle = await Article.create(article);
+  res.send("New article created!");
+}
+
+//Update article by ID
+async function updateArticle(req, res) {
+  if (!req.body.title || !req.body.content) {
+    res.send("You need to provide a title and some content.");
+  }
+
+  const checkArticle = await Article.findByPk(req.params.id);
+
+  if (checkArticle) {
+    const updatedArticle = await Article.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.send(`Article with ID: ${req.params.id} successfully updated.`);
+  } else {
+    res.send("There's no article with that ID.");
+  }
+}
+
+//Delete article
+async function deleteArticle(req, res) {
+  const checkArticle = await Article.findByPk(req.params.id);
+
+  if (checkArticle) {
+    const deletedArticle = await Article.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.send(`Article with ID: ${req.params.id} successfully deleted.`);
+  } else {
+    res.send("There's no article with that ID.");
+  }
+}
+
+//USERS
 //Show all users
 async function indexUsers(req, res) {
   const users = await User.findAll({
@@ -20,33 +96,71 @@ async function indexUsers(req, res) {
   res.json(users);
 }
 
-//Filter by Author(User)
-async function showByUserId(req, res) {
-  const articles = await Article.findAll({
-    include: User,
-    where: { userId: req.params.id },
-    raw: true,
-    order: [["createdAt", "DESC"]],
-  });
-  res.json(articles);
+//Create new user
+async function createUser(req, res) {
+  if (
+    !req.body.firstname ||
+    !req.body.lastname ||
+    !req.body.email ||
+    !req.body.password
+  ) {
+    res.send(
+      "You need to provide the following fields: firstname, lastname, email and password."
+    );
+  }
+  const { firstname, lastname, email, password } = req.body;
+  const user = {
+    firstname,
+    lastname,
+    email,
+    password,
+  };
+
+  const newUser = await User.create(user);
+  res.send("New user created!");
 }
 
-//Filter by word/letters
-async function showByUserId(req, res) {
-  const articles = await Article.findAll({
-    where: {
-      title: {
-        [Op.substring]: [req.params.word],
+//Update user by ID
+async function updateUser(req, res) {
+  const checkUser = await User.findByPk(req.params.id);
+
+  if (checkUser) {
+    const updatedUser = await User.update(req.body, {
+      where: {
+        id: req.params.id,
       },
-    },
-    raw: true,
-    order: [["createdAt", "DESC"]],
-  });
-  res.json(articles);
+    });
+    res.send(`User with ID: ${req.params.id} successfully updated.`);
+  } else {
+    res.send("There's no user with that ID.");
+  }
+}
+
+//Delete user
+async function deleteUser(req, res) {
+  const checkArticle = await User.findByPk(req.params.id);
+
+  if (checkArticle) {
+    const udeletedUser = await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.send(`User with ID: ${req.params.id} successfully deleted.`);
+  } else {
+    res.send("There's no article with that ID.");
+  }
 }
 
 module.exports = {
   indexArticles,
-  showByUserId,
+  createArticle,
+  updateArticle,
+  deleteArticle,
+  FindArticleByUserId,
+  findArticleByTitle,
   indexUsers,
+  createUser,
+  updateUser,
+  deleteUser,
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,16 +2,39 @@ const express = require("express");
 const router = express.Router();
 const apiController = require("../controllers/apiController");
 
+//ARTICLES
 //Show all articles
 router.get("/articles", apiController.indexArticles);
 
+//Show article by UserId
+router.get("/articles/user/:id", apiController.showByUserId);
+
+//Show by title
+router.get("/articles/:title", apiController.findArticleByTitle);
+
+//Create article
+router.post("/articles", apiController.createArticle);
+
+//Update article
+router.put("/articles/:id", apiController.updateArticle);
+
+//Delete article
+router.delete("articles/:id", apiController.deleteArticle);
+
+//USERS
 //Show all users
 router.get("/users", apiController.indexUsers);
 
-//Show by UserId
-router.get("/:id", apiController.showByUserId);
+//Show user by ID
+router.get("/users/:id");
 
-//Show by word/letters
-router.get("/search/:word", apiController.showByUserId);
+//Create user
+router.post("/users", apiController.createUser);
+
+//Update user
+router.put("/users:id", apiController.updateUser);
+
+//Delete user
+router.delete("/users/:id", apiController.deleteUser);
 
 module.exports = router;
